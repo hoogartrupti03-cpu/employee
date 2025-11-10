d5a025b17bb86ce3039222270fbda5326e3865d3
hoogartrupti03-cpu <hoogartrupti03@gmail.com>
2025-11-10 15:55:22 +0530

    Create employee.py

---
+++ employee.py
@@ -0,0 +1,7 @@
+salary = float(input("Enter employee's salary: "))
+
+bonus = salary * 0.10
+total_salary = salary + bonus
+
+print("Bonus Amount =", bonus)
+print("Total Salary after Bonus =", total_salary)
